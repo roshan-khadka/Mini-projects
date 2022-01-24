9845faa3ead2d6b1366da84d34e007d89e0c2eed
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-24 10:59:01 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,18 +1,57 @@
-#: A TicTacToe game
+#: A classic TicTacToe game
 
-board = {'top-L': 'x', 'top-M': 'o', 'top-R': 'x',
-         'mid-L': 'x', 'mid-M': 'x', 'mid-R': 'o',
-         'low-L': 'o', 'low-M': 'o', 'low-R': 'x'}
+# ------Global Variables------
+board = ["1", "2", "3",
+        "4", "5", "6",
+        "7", "8", "9"]
+game_status = True
+winner = None
+current_player = "X"
 
-def board_display(board):
-   print(board['top-L'] + ' | ' + board['top-M'] + ' | ' + board['top-R'] + ' | ')
+def board_display():
+   print(board[0] + ' | ' + board[1] + ' | ' + board[2] + ' | ')
    print('------------')
-   print(board['mid-L'] + ' | ' + board['mid-M'] + ' | ' + board['mid-R'] + ' | ')
+   print(board[3] + ' | ' + board[4] + ' | ' + board[5] + ' | ')
    print('-------------')
-   print(board['low-L'] + ' | ' + board['low-M'] + ' | ' + board['low-R'] + ' | ')
+   print(board[6] + ' | ' + board[7] + ' | ' + board[8] + ' | ')
     
-board_display(board)
+def start_game():
+    board_display()
+    # while the game is still running
+    while game_status:
+        
+        player_input()
+        check_if_game_over()
+        switch_player()
+        
+        if winner == "X" or winner == "0":
+            print(winner + "won.")
+        elif winner == None:
+            print("Tie.")
 
-# next turn player 1 vs player 2
-# check win - coulumn, row, diagonal
-# play again, result, exit
+def check_if_game_over():
+    check_win()
+    check_tie()
+
+def check_win():
+    # Check rows, columns, and diagonal
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
+    return
+
+def check_rows():
+    return
+
+def check_columns():
+    return
+
+def check_diagonals():
+    return
+
+def check_tie():
+    return
+
+def switch_player():
+    return
+         
