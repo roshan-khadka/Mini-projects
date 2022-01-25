7033e3de5038dac6d18d03d5ee510613d3ded698
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-25 10:04:58 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -41,12 +41,56 @@ def check_win():
     return
 
 def check_rows():
+    global game_status
+    # Check if the rows are same
+    row_1 = board[0] == board[1] == board[2]
+    row_2 = board[3] == board[4] == board[5]
+    row_3 = board[6] == board[7] == board[8]
+
+    if row_1 or row_2 or row_3:
+      game_status = False
+
+    if row_1:
+      return board[0]
+    elif row_2:
+      return board[3]
+    elif row_3:
+      return board[6]
+    return
     return
 
 def check_columns():
+    global game_status
+    # Check if the rows are same
+    column_1 = board[0] == board[3] == board[6]
+    column_2 = board[1] == board[4] == board[7]
+    column_3 = board[2] == board[5] == board[8]
+
+    if column_1 or column_2 or column_3:
+      game_status = False
+
+    if column_1:
+      return board[0]
+    elif column_2:
+      return board[1]
+    elif column_3:
+      return board[2]
+    return
     return
 
 def check_diagonals():
+    global game_status
+    # Check if the rows are same
+    diagonal_1 = board[0] == board[4] == board[8]
+    diagonal_2 = board[3] == board[4] == board[6]
+
+    if diagonal_1 or diagonal_2:
+      game_status = False
+
+    if row_1:
+      return board[0]
+    elif row_2:
+      return board[3]
     return
 
 def check_tie():
