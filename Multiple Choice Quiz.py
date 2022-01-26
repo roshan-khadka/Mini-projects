a534aac5d983da37389d113b3aeac99113ce8ccf
Roshan Khadka <65776857+roshan-khadka@users.noreply.github.com>
2022-01-26 09:42:11 +0200

    Create Multiple Choice Quiz.py

---
+++ Multiple Choice Quiz.py
@@ -0,0 +1,9 @@
+#: A simple multiple choice quiz, implementing the oop.
+
+from Class_Question import Question
+
+questions_prompt  = [
+    "1. What color are apples?\n (a) Red\n (b) Green\n (c) Orange\n",
+    "2. What color are bananas?\n (a) Red\n (b) Yellow\n (c) Orange\n",
+    "3. What color are strawberries?\n (a) Red\n (b) Green\n (c) Orange\n"
+    ]
